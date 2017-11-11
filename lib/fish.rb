7443b7808dcc80e215250a2186c5f37ab65c0bf6
Nolan Hughes <nhughes987@gmail.com>
2017-11-11 19:24:48 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
+# 
+# fish = Fish.new("Bubbles")
+#
+# binding.pry
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,59 @@
+require 'pry'
+require_relative 'fish.rb'
 class Owner
   # code goes here
+  attr_reader :species
+  attr_accessor :name
+
+  @@all_owners = []
+
+  def initialize(species)
+    @species = species
+    @@all_owners << self
+  end
+
+  def self.all
+    @@all_owners
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    @@all_owners.count
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  @@pet_hash = {:fishes => [], :dogs => [], :cats => []}
+
+  def pets
+    @@pet_hash
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @@pet_hash[:fishes] = [new_fish]
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @@pet_hash[:cats] = [new_cat]
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @@pet_hash[:dogs] = [new_dog]
+  end
+
+  def walk_dogs
+    
+  end
+
 end
+
+# owner = Owner.new("human")
+# binding.pry
