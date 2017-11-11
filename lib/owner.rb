62754788fd445be24cb1ba12f030f16e17bed9e5
Nolan Hughes <nhughes987@gmail.com>
2017-11-11 21:02:53 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
-require 'pry'
-require_relative 'fish.rb'
-require_relative 'dog.rb'
-require_relative 'cat.rb'
+# require 'pry'
+# require_relative 'fish.rb'
+# require_relative 'dog.rb'
+# require_relative 'cat.rb'
 class Owner
   attr_reader :species
   attr_accessor :name
@@ -99,16 +99,16 @@ class Owner
 
 end
 
-owner = Owner.new("human")
-# fido = Dog.new("Fido")
-# tabby = Cat.new("Tabby")
-# nemo = Fish.new("Nemo")
-# [fido, tabby, nemo].each {|o| o.mood = "happy" }
-# owner.pets = {
-#   :dogs => [fido, Dog.new("Daisy")],
-#   :fishes => [nemo],
-#   :cats => [Cat.new("Mittens"), tabby]
-# }
-
-texas = owner.buy_dog("Texas")
-binding.pry
+# owner = Owner.new("human")
+# # fido = Dog.new("Fido")
+# # tabby = Cat.new("Tabby")
+# # nemo = Fish.new("Nemo")
+# # [fido, tabby, nemo].each {|o| o.mood = "happy" }
+# # owner.pets = {
+# #   :dogs => [fido, Dog.new("Daisy")],
+# #   :fishes => [nemo],
+# #   :cats => [Cat.new("Mittens"), tabby]
+# # }
+# 
+# texas = owner.buy_dog("Texas")
+# binding.pry
