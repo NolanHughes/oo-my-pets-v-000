77e90108cad59b35512f05fc0f23f15adc73c3bc
Nolan Hughes <nhughes987@gmail.com>
2017-11-11 21:04:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
-# require 'pry'
-# require_relative 'fish.rb'
-# require_relative 'dog.rb'
-# require_relative 'cat.rb'
+require 'pry'
+require_relative 'fish.rb'
+require_relative 'dog.rb'
+require_relative 'cat.rb'
 class Owner
   attr_reader :species
   attr_accessor :name
