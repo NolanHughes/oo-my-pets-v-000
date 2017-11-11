95c61b248198fa1be9214990752996f99b478aa7
Nolan Hughes <nhughes987@gmail.com>
2017-11-11 20:21:35 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -85,14 +85,14 @@ class Owner
 
 end
 
-owner = Owner.new("human")
-fido = Dog.new("Fido")
-tabby = Cat.new("Tabby")
-nemo = Fish.new("Nemo")
-melkor = Cat.new("Melkor")
-[fido, tabby, nemo, melkor].each {|o| o.mood = "happy" }
-owner.pets[:dogs] += [fido]
-owner.pets[:cats] += [tabby, melkor]
-owner.pets[:fishes] += [nemo]
-
-binding.pry
+# owner = Owner.new("human")
+# fido = Dog.new("Fido")
+# tabby = Cat.new("Tabby")
+# nemo = Fish.new("Nemo")
+# melkor = Cat.new("Melkor")
+# [fido, tabby, nemo, melkor].each {|o| o.mood = "happy" }
+# owner.pets[:dogs] += [fido]
+# owner.pets[:cats] += [tabby, melkor]
+# owner.pets[:fishes] += [nemo]
+# 
+# binding.pry
