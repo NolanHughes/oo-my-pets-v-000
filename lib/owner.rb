6521c005c6805205e99d99a6366fb3b63f72e014
Nolan Hughes <nhughes987@gmail.com>
2017-11-11 20:51:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -76,9 +76,9 @@ class Owner
   end
 
   def sell_pets
-    @@pet_hash.each do |key, value|
-        value.each { |pets_object| pets_object.mood = "nervous" }
-    end
+    # @@pet_hash.each do |key, value|
+    #     value.each { |pets_object| pets_object.mood = "nervous" }
+    # end
     # @@pet_hash.each do |key, value|
     #     value.clear
     # end
